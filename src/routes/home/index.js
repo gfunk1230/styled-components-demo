a14c339a06f55f7eed55e1f5ba22e370eac37240
Greg Campoverde <gcampoverde@grubhub.com>
2019-09-30 15:26:45 -0400

    Added global styles with fonts

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,11 +1,13 @@
 import { h } from "preact";
 import style from "./style";
+import GlobalStyles from "../../components/globalStyles/";
 
 import { Button, PrimaryButton } from "../../components/primaryButton/button";
 import { radius5, radius10, radius25 } from "../../components/tokens";
 
 const Home = () => (
   <main class={style.home}>
+    <GlobalStyles/>
     <h1>Primary Button Samples</h1>
     <p>
       <strong>Props :</strong> radius, disabled , size, color
