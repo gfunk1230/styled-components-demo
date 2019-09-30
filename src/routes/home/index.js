fea2700db7d4045f82a0e84313db5670b9a76d2f
Greg Campoverde <gcampoverde@grubhub.com>
2019-09-30 14:47:31 -0400

    Updated tokens file with latest colors

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -2,7 +2,7 @@ import { h } from "preact";
 import style from "./style";
 
 import { Button, PrimaryButton } from "../../components/primaryButton/button";
-import { radiusOne, radiusTwo, radiusThree } from "../../components/tokens";
+import { radius5, radius10, radius25 } from "../../components/tokens";
 
 const Home = () => (
   <main class={style.home}>
@@ -11,16 +11,16 @@ const Home = () => (
       <strong>Props :</strong> radius, disabled , size, color
     </p>
     <section class={style.buttonWrapper}>
-      <PrimaryButton as={Button} size="sm" color="red" radius={radiusOne}>
+      <PrimaryButton as={Button} size="sm" color="blue" radius={radius5}>
         Primary
       </PrimaryButton>
-      <PrimaryButton as={Button} size="md" color="orange" radius={radiusTwo}>
+      <PrimaryButton as={Button} size="md" color="orange" radius={radius10}>
         Primary
       </PrimaryButton>
-      <PrimaryButton as={Button} size="lg" color="blue" radius={radiusThree}>
+      <PrimaryButton as={Button} size="lg" color="blue" radius={radius25}>
         Primary
       </PrimaryButton>
-      <PrimaryButton as={Button} size="lg" color="blue" radius={radiusThree} disabled>
+      <PrimaryButton as={Button} size="lg" color="blue" radius={radius10} disabled>
         Disabled
       </PrimaryButton>
     </section>
