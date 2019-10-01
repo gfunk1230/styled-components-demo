c5e2e625dd29c365cc7191da98da614fb71e4b65
Greg Campoverde <gcampoverde@grubhub.com>
2019-10-01 14:03:53 -0400

    Added typography component

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -4,8 +4,9 @@ import styled from 'styled-components';
 import GlobalStyles from "../../components/globalStyles/";
 
 import { Button, PrimaryButton } from "../../components/primaryButton/button";
-import GridParent from "../../components/Grid/gridParent";
-import GridChild from "../../components/Grid/gridChild";
+import GridParent from "../../components/grid/gridParent";
+import GridChild from "../../components/grid/gridChild";
+import Typography from "../../components/type";
 import { media } from "../../components/utils/mediaQueries";
 
 import { radius5, radius10, radius25 } from "../../components/tokens";
@@ -39,41 +40,67 @@ const Home = () => (
         </PrimaryButton>
       </div>
     </section>
+    <hr></hr>
 
     <section>
       <h2>Grid System Samples</h2>
-      <GridParent gridTemplateColumns={12} bgColor="red">
-        <GridChild desktop={2} bgColor="green">
+      <p>
+        <strong>Props :</strong> bgColor, phone , tablet, desktop
+      </p>
+      <GridParent gridTemplateColumns={12} bgColor="blueviolet">
+        <GridChild as="span" desktop={2} bgColor="aqua">
           <img src="https://placeimg.com/550/550/any"/>
           <p>2 column div on desktop</p>
         </GridChild>
-        <GridChild desktop={2} bgColor="green">
+        <GridChild desktop={2} bgColor="aqua">
           <img src="https://placeimg.com/550/550/any"/>
           <p>2 column div on desktop</p>
         </GridChild>
-        <GridChild desktop={2} bgColor="green">
+        <GridChild desktop={2} bgColor="aqua">
           <img src="https://placeimg.com/550/550/any"/>
           <p>2 column div on desktop</p>
         </GridChild>
-        <GridChild desktop={2} bgColor="green">
+        <GridChild desktop={2} bgColor="aqua">
           <img src="https://placeimg.com/550/550/any"/>
           <p>2 column div on desktop</p>
         </GridChild>
-        <GridChild desktop={2} bgColor="green">
+        <GridChild desktop={2} bgColor="aqua">
           <img src="https://placeimg.com/550/550/any"/>
           <p>2 column div on desktop</p>
         </GridChild>
-        <GridChild desktop={2} bgColor="green">
+        <GridChild desktop={2} bgColor="aqua">
           <img src="https://placeimg.com/550/550/any"/>
           <p>2 column div on desktop</p>
         </GridChild>
       </GridParent>
     </section>
+    <hr></hr>
 
     <section>
+      <h2>Media Queries Utility</h2>
+      <p>
+        <strong>Props :</strong> phone , tablet, desktop
+      </p>
       <HeaderResponsive>
         Bacon ipsum dolor amet ground round cow short ribs ham hock boudin buffalo, kielbasa drumstick beef ribs. Pancetta chicken venison, flank short ribs turducken ham hock prosciutto. Salami boudin ball tip capicola, venison landjaeger buffalo swine short loin pastrami jerky. Shoulder sausage rump tongue ball tip brisket drumstick pastrami beef ribs turkey cow pork belly filet mignon hamburger ribeye. Short ribs spare ribs tri-tip kielbasa turducken chuck.
       </HeaderResponsive>
+    </section>
+    <hr></hr>
+
+    <section>
+      <h2>Typography</h2>
+      <p>
+        <strong>Props :</strong> component, variant, flush
+      </p>
+      <Typography component="h1" variant="display-1">
+        Hey there!
+      </Typography>
+      <Typography component="h2" variant="display-2">
+        Hey there!
+      </Typography>
+      <Typography component="h3" variant="body-1-bold">
+        Hey there!
+      </Typography>
     </section>
   </main>
 );
